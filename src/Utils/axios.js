ca64d2061faf1e3312bf63ef855f107916a8f8e0
Okekeprince1 <okeke.p.ebube@gmail.com>
2020-07-11 15:25:14 +0100

    Added api key from config folder to axios header

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
+import { API_KEY } from '../config';
 
-
-axios.defaults.headers.common['X-CMC_PRO_API_KEY'] = ;
+axios.defaults.headers.common['X-CMC_PRO_API_KEY'] = API_KEY ;
 
 export default axios;
