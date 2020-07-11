c1a634fab8d4bd6e7770737ec19eff53e3c808f3
Okekeprince1 <okeke.p.ebube@gmail.com>
2020-07-11 15:23:44 +0100

    Removed api key

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.headers.common['X-CMC_PRO_API_KEY'] = '61ff1fa6-d6b3-4b4c-82ac-058599d1d254';
+
+axios.defaults.headers.common['X-CMC_PRO_API_KEY'] = ;
 
 export default axios;
